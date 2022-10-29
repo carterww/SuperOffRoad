7ce8a227415cd87686f8f8fffa442e9283cd7a47
carterww <carterww@hotmail.com>
2022-10-29 16:09:06 -0500

    Update TruckController.cs

--- Assets/Scripts/TruckController.cs
+++ Assets/Scripts/TruckController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public abstract class TruckController : MonoBehaviour
 {
-    TruckControllerImp implementation;
+    // Need to implement factory method here to choose
+    // Hard coded variable for testing
+    TruckControllerImp implementation = new PlayerTruckController();
 
     // Start is called before the first frame update
     void Start()
