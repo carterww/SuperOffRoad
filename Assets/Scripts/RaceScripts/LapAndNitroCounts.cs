60419cc040ab23dccc74b90af7931f635a1c1000
carterww <carterww@hotmail.com>
2022-11-21 18:18:54 -0600

    Changed Lap and nitro count script

--- Assets/Scripts/RaceScripts/LapAndNitroCounts.cs
+++ Assets/Scripts/RaceScripts/LapAndNitroCounts.cs
@@ -6,21 +6,19 @@ using UnityEngine.UI;
 public class LapAndNitroCounts : MonoBehaviour
 {
     Text text;
-    TruckController[] trucks = new TruckController[4];
+    TruckController truck;
+    public string TruckName;
 
     // Start is called before the first frame update
     void Start()
     {
         text = GetComponent<Text>();
-        trucks[0] = GameObject.Find("Truck").GetComponent<TruckController>();
-        trucks[1] = GameObject.Find("Truck (1)").GetComponent<TruckController>();
-        trucks[2] = GameObject.Find("Truck (2)").GetComponent<TruckController>();
-        trucks[3] = GameObject.Find("Truck (3)").GetComponent<TruckController>();
+        truck = GameObject.Find(TruckName).GetComponent<TruckController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        text.text = $"{trucks[0].lapCount}  {trucks[1].lapCount}  {trucks[2].lapCount}  {trucks[3].lapCount}\n{trucks[0].nitroCount} {trucks[1].nitroCount} {trucks[2].nitroCount} {trucks[3].nitroCount}";
+        text.text = $"{truck.lapCount}\n{truck.nitroCount}";
     }
 }
