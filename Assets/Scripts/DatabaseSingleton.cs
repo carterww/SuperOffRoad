537d1df2e0e40d74d92554382a692fd71e1d72f3
Trevor Kerney <trevorkerney@hotmail.com>
2022-11-29 05:34:23 -0600

    database login/logout

--- Assets/Scripts/DatabaseSingleton.cs
+++ Assets/Scripts/DatabaseSingleton.cs
@@ -8,7 +8,8 @@ public class DatabaseSingleton : MonoBehaviour
 {
     private static IDbConnection conn;
     private static string[] tables = {
-        "Highscores (initials CHAR(3) PRIMARY KEY, time INTEGER)"
+        "Account (username CHAR(16) PRIMARY KEY, password CHAR(32))",
+        "Highscore (initials CHAR(3) PRIMARY KEY, time INTEGER)",
     };
 
     // Start is called before the first frame update
@@ -32,7 +33,37 @@ public class DatabaseSingleton : MonoBehaviour
 
     public void Highscore(string initials, int time) {
         IDbCommand newScoreComm = conn.CreateCommand();
-        newScoreComm.CommandText = "INSERT OR REPLACE INTO Highscores (initials, time) VALUES ('" + initials + "', '" + time + "')"; 
+        newScoreComm.CommandText = "INSERT OR REPLACE INTO Highscore (initials, time) VALUES ('" + initials + "', '" + time + "')"; 
         newScoreComm.ExecuteNonQuery();
+    }
+
+    public bool CheckUsername(string username) {
+        IDbCommand readUsernamesComm = conn.CreateCommand();
+        readUsernamesComm.CommandText = "SELECT * FROM Account;";
+        IDataReader dataReader = readUsernamesComm.ExecuteReader();
+        while (dataReader.Read()) if (username.Equals(dataReader.GetString(0))) return false;
+        return true;
+    }
+
+    public void CreateUser(string username, string password) {
+        Hash128 hash = new Hash128();
+        hash.Append(password);
+        IDbCommand newScoreComm = conn.CreateCommand();
+        newScoreComm.CommandText = "INSERT OR REPLACE INTO Account (username, password) VALUES ('" + username + "', '" + hash.ToString() + "')"; 
+        if (newScoreComm.ExecuteNonQuery() != 1) Debug.Log("There was an unexpected error in CreateUser.");
+    }
+
+    public bool LoginUser(string username, string password) {
+        Hash128 hash = new Hash128();
+        hash.Append(password);
+        IDbCommand readUsernamesComm = conn.CreateCommand();
+        readUsernamesComm.CommandText = "SELECT * FROM Account;";
+        IDataReader dataReader = readUsernamesComm.ExecuteReader();
+        while (dataReader.Read()) {
+            if (username.Equals(dataReader.GetString(0)) && hash.ToString().Equals(dataReader.GetString(1))) {
+                return true;
+            }
+        }
+        return false;
     }
 }
